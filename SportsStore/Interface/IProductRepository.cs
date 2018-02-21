bed1eabd3aa39e85ca2105f4d26950e5edf4d8ae
WillB01 <wb.william@outlook.com>
2018-02-21 14:48:04 +0100

    Pro ASP.NET Core MVC 2 SportsStore

---
+++ SportsStore/Interface/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Interface
+{
+    interface IProductRepository
+    {
+        //IEnumerable<Product> Products { get;  }
+
+        IQueryable<Product> Products { get; }
+    }
+}
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -15,6 +15,7 @@ namespace SportsStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +25,13 @@ namespace SportsStore
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+
+            });
 
             app.Run(async (context) =>
             {
