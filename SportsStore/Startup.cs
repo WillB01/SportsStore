81250bce633c6839ff17e6a42de18a6caf837f3f
WillB01 <wb.william@outlook.com>
2018-02-21 20:54:56 +0100

    adding database

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -5,7 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SportsStore.Data;
 using SportsStore.Interface;
 using SportsStore.Models;
 
@@ -13,11 +16,18 @@ namespace SportsStore
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+       
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration["Data:SportStoreProducts"])); 
+            services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc();
         }
 
