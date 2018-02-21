15ce879e8052d1360332220db284624becec50e3
WillB01 <wb.william@outlook.com>
2018-02-21 20:37:52 +0100

    adding stuff

---
+++ SportsStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ SportsStore/Models/EFProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using SportsStore.Data;
+using SportsStore.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private ApplicationDbContext _dbcontext;
+
+        public EFProductRepository(ApplicationDbContext ctx)
+        {
+            _dbcontext = ctx;
+        }
+
+        public IQueryable<Product> Products => _dbcontext.Products;
+    }
+
+    
+}
